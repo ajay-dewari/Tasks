e199018c87a55bfc49d67539565e686385cea71f
ajaydewari <ajdewari@gmail.com>
2021-07-08 10:29:52 +0530

    tasks list observed in TasksFragment

--- app/src/main/java/me/ajay/tasks/ui/tasks/TasksFragment.kt
+++ app/src/main/java/me/ajay/tasks/ui/tasks/TasksFragment.kt
@@ -1,11 +1,32 @@
 package me.ajay.tasks.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import me.ajay.tasks.R
+import me.ajay.tasks.databinding.FragmentTasksBinding
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
     private val tasksViewModel : TasksViewModel by viewModels()
+    private val tasksAdapter = TasksAdapter()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentTasksBinding.bind(view)
+
+        tasksViewModel.tasks.observe(viewLifecycleOwner) {
+            tasksAdapter.submitList(it)
+        }
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = tasksAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+    }
 }
--- app/src/main/java/me/ajay/tasks/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/me/ajay/tasks/ui/tasks/TasksViewModel.kt
@@ -1,10 +1,15 @@
 package me.ajay.tasks.ui.tasks
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import me.ajay.tasks.data.Task
+import me.ajay.tasks.data.TasksDao
 import javax.inject.Inject
 
 @HiltViewModel
-class TasksViewModel @Inject constructor() : ViewModel() {
+class TasksViewModel @Inject constructor(private val tasksDao : TasksDao) : ViewModel() {
 
+    val tasks = tasksDao.getTasks().asLiveData()
 }
