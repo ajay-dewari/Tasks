e8a697900357593b197bd775f878bb9fce8970a0
ajaydewari <ajdewari@gmail.com>
2021-07-08 07:25:41 +0530

    dao and database class added

---
+++ app/src/main/java/me/ajay/tasks/data/TasksDao.kt
@@ -0,0 +1,25 @@
+package me.ajay.tasks.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TasksDao {
+
+    @Query("SELECT * FROM task_table")
+    fun getTasks(): Flow<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Delete)
+}
